5fac47d720a17f78ca4595e213ccf6cca898b029
smritinaik <148971067+smritinaik@users.noreply.github.com>
2026-06-03 12:25:31 +0530

    nav tab dummy
    nav tab dummy

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,39 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
 
-export default function RootLayout() {
-  return <Stack />;
+export default function Layout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+        }}
+      />
+
+      <Tabs.Screen
+        name="add"
+        options={{
+          title: "Add",
+        }}
+      />
+
+      <Tabs.Screen
+        name="streaks"
+        options={{
+          title: "Streaks",
+        }}
+      />
+
+      <Tabs.Screen
+        name="garden"
+        options={{
+          title: "Garden",
+        }}
+      />
+    </Tabs>
+  );
 }
